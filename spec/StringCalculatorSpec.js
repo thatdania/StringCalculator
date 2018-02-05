15348466b4326a9ed14ca94e2fbd4b5b41fba9a9
thatdania@gmail.com <thatdania@gmail.com>
2018-02-05 16:07:33 +0000

    Create test for if no arguments are given, it returns 0
    test fails

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,7 +1,13 @@
 describe("StringCalculator", function() {
 
-  it('should initalise', function(){
+  beforeEach(function() {
     stringCalculator = new StringCalculator
+  });
+
+  describe("#Caculate", function(){
+    it('should return 0 if the method is not given any arguments', function(){
+      expect(stringCalculator.calculate()).toEqual(0)
+    });
   });
 
 });
